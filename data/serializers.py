e991a0e1e978443efade118cec59fca05193f445
Jeff Mitchell <jeffmitchell33@gmail.com>
2023-08-25 17:36:15 +0200

    Working table logic complete

--- data/serializers.py
+++ data/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from .models import *
 
-
 class EternalPowersSerializer(serializers.ModelSerializer):
     class Meta:
         model = Eternal_Powers
@@ -9,9 +8,17 @@ class EternalPowersSerializer(serializers.ModelSerializer):
 
 
 class ItemSerializer(serializers.ModelSerializer):
+    parent_monster_name = serializers.PrimaryKeyRelatedField(
+        queryset=Monster.objects.all(), source='short', write_only=True
+    )
+
+    parent_area_name = serializers.PrimaryKeyRelatedField(
+        queryset=Area.objects.all(), source='name', write_only=True
+    )
+
     class Meta:
         model = Item
-        fields = ('id', 'parent_monster', 'parent_area', 'short', 'description', 'slot', 'weight', 'unbreakable', 'cursed', 'hp_regen', 'sp_regen', 'hp_drain', 'sp_drain', 'ftouchable', 'transmutable', 'duplicable', 'bind_on_pickup', 'bind_on_equip', 'stats_str', 'stats_con', 'stats_int', 'stats_wis', 'stats_dex', 'stats_cha', 'smd', 'specials', 'edged', 'blunt', 'fire', 'ice', 'acid', 'electricity', 'mind', 'energy', 'poison', 'radiation')
+        fields = ('id', 'parent_monster', 'parent_area', 'parent_monster_name', 'parent_area_name', 'short', 'description', 'slot', 'weight', 'unbreakable', 'cursed', 'hp_regen', 'sp_regen', 'hp_drain', 'sp_drain', 'ftouchable', 'transmutable', 'duplicable', 'bind_on_pickup', 'bind_on_equip', 'stats_str', 'stats_con', 'stats_int', 'stats_wis', 'stats_dex', 'stats_cha', 'smd', 'specials', 'edged', 'blunt', 'fire', 'ice', 'acid', 'electricity', 'mind', 'energy', 'poison', 'radiation')
 
 class AreaSerializer(serializers.ModelSerializer):
     class Meta:
@@ -19,11 +26,19 @@ class AreaSerializer(serializers.ModelSerializer):
         fields = ('id', 'name', 'realm', 'dungeon')
 
 class MonsterSerializer(serializers.ModelSerializer):
+    parent_monster_name = serializers.PrimaryKeyRelatedField(
+        queryset=Area.objects.all(), source='name', write_only=True
+    )
+
     class Meta:
         model = Monster
-        fields = ('id', 'parent_area', 'short', 'description', 'size', 'notes', 'scaler')
+        fields = ('id', 'parent_area', 'parent_area_name', 'short', 'description', 'size', 'notes', 'scaler')
 
 class MonsterAttackSerializer(serializers.ModelSerializer):
+    parent_monster_name = serializers.PrimaryKeyRelatedField(
+        queryset=Monster.objects.all(), source='short', write_only=True
+    )
+
     class Meta:
         model = Monster_AttackType
-        fields = ('id', 'parent_monster', 'damage_type', 'attack_type')
+        fields = ('id', 'parent_monster', 'parent_monster_name', 'damage_type', 'attack_type')
--- static/scripts/javascript/tables.js
+++ static/scripts/javascript/tables.js
@@ -1,27 +1,96 @@
-
-async function getAreaData() {
-  const response = await fetch("https://129.213.48.136/api/area/");
-  const areaData = await response.json();
-  return areaData.results;
-}
-
-const apiAreaData = getAreaData();
-
-console.log('data is', apiAreaData);
-
-
-//Build Tabulator
-let table = new Tabulator("#example-table", {
-  layout:"fitColumns",
-  placeholder:"No Data Set",
-  columns:[
-      {title:"Name", field:"name", sorter:"string", width:200},
-      {title:"Realm", field:"realm", sorter:"string"},
-  ],
-});
-
-
-//trigger AJAX load on "Load Data via AJAX" button click
+//trigger AJAX load on selected button clicks;
 document.getElementById("area-trigger").addEventListener("click", function(){
-  table.setData("https://129.213.48.136/api/area/");
+  let table = new Tabulator("#data-table", {
+    layout:"fitColumns",
+    placeholder:"No Data Set",
+    columns:[
+        {title:"Name", field:"name", sorter:"string", width:200},
+        {title:"Realm", field:"realm", sorter:"string"},
+    ],
+  });
+
+  fetch('https://129.213.48.136/api/area/')
+    .then(response => response.json())
+    .then(data => {
+      const areaData = data.results;
+      table.setData(areaData);
+    })
+    .catch(error => console.error(error));  
 });
+
+document.getElementById("monster-trigger").addEventListener("click", function(){
+  let table = new Tabulator("#data-table", {
+    layout:"fitColumns",
+    placeholder:"No Data Set",
+    columns:[
+        {title:"Name", field:"short", sorter:"string"},
+        {title:"Class", field:"size", sorter:"number"},
+        {title:"Area", field:"parent_area_name", sorter:"string"},
+        {title:"Scaler", field:"scaler", sorter:"number"},
+        {title:"Description", field:"description"},
+        {title:"Notes", field:"notes", sorter:"string"},
+    ],
+  });
+
+  fetch('https://129.213.48.136/api/monster/')
+    .then(response => response.json())
+    .then(data => {
+      const areaData = data.results;
+      table.setData(areaData);
+    })
+    .catch(error => console.error(error));  
+});
+
+document.getElementById("item-trigger").addEventListener("click", function(){
+  let table = new Tabulator("#data-table", {
+    layout:"fitColumns",
+    placeholder:"No Data Set",
+    columns: [        
+      {title:"Name", field:"short", sorter:"string"},
+      {title:"Area", field:"parent_area_name", sorter:"string"},
+      {title:"Mob", field:"parent_monster_name", sorter:"string"},
+      {title:"Type", field:"slot", sorter:"string"},
+      {title:"Weight", field:"weight", sorter:"string"},
+      {title:"Unbreakable", field:"unbreakable", sorter:"bool"},
+      {title:"Cursed", field:"cursed", sorter:"bool"},
+      {title:"Description", field:"description", sorter:"string"},
+      {title: "HP Regen" field: "hp_regen", sorter: "number"},
+      {title: "SP Regen" field: "sp_regen", sorter: "number"},
+      {title: "HP Drain" field: "hp_drain", sorter: "number"},
+      {title: "SP Drain" field: "sp_drain", sorter: "number"},
+      {title: "Ftouch?" field: "ftouchable", sorter: "bool"},
+      {title: "Transmute" field: "transmutable", sorter: "bool"},
+      {title: "Dupe Item" field: "duplicable", sorter: "bool"},
+      {title: "BOP" field: "bind_on_pickup", sorter: "bool"},
+      {title: "BOE" field: "bind_on_equip", sorter: "bool"},
+      {title: "stats_str" field: "stats_str", sorter: "number"},
+      {title: "stats_con" field: "stats_con", sorter: "number"},
+      {title: "stats_int" field: "stats_int", sorter: "number"},
+      {title: "stats_wis" field: "stats_wis", sorter: "number"},
+      {title: "stats_dex" field: "stats_dex", sorter: "number"},
+      {title: "stats_cha" field: "stats_cha", sorter: "number"},
+      {title: "SMD" field: "smd", sorter: "bool"},
+      {title: "edged" field: "edged", sorter: "number"},
+      {title: "blunt" field: "blunt", sorter: "number"},
+      {title: "fire" field: "fire", sorter: "number"},
+      {title: "ice" field: "ice", sorter: "number"},
+      {title: "acid" field: "acid", sorter: "number"},
+      {title: "electricity" field: "electricity", sorter: "number"},
+      {title: "mind" field: "mind", sorter: "number"},
+      {title: "energy" field: "energy", sorter: "number"},
+      {title: "poison" field: "poison", sorter: "number"},
+      {title: "radiation" field: "radiation", sorter: "number"},
+      {title: "Specials" field: "specials", sorter: "string"},
+    ],
+  });
+
+  fetch('https://129.213.48.136/api/item/')
+    .then(response => response.json())
+    .then(data => {
+      const areaData = data.results;
+      table.setData(areaData);
+    })
+    .catch(error => console.error(error));  
+});
+
+
