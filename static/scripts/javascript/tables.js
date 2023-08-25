08c72c017accc2c5eacbfc4299b950e185076e83
Jeff Mitchell <jeffmitchell33@gmail.com>
2023-08-25 16:29:53 +0200

    added area button

--- static/scripts/javascript/tables.js
+++ static/scripts/javascript/tables.js
@@ -1,10 +1,14 @@
-import {TabulatorFull as Tabulator} from 'tabulator-tables';
 
 async function getAreaData() {
   const response = await fetch("https://129.213.48.136/api/area/");
   const areaData = await response.json();
   return areaData.results;
 }
+
+const apiAreaData = getAreaData();
+
+console.log('data is', apiAreaData);
+
 
 //Build Tabulator
 let table = new Tabulator("#example-table", {
@@ -14,5 +18,10 @@ let table = new Tabulator("#example-table", {
       {title:"Name", field:"name", sorter:"string", width:200},
       {title:"Realm", field:"realm", sorter:"string"},
   ],
-  data: getAreaData()
+});
+
+
+//trigger AJAX load on "Load Data via AJAX" button click
+document.getElementById("area-trigger").addEventListener("click", function(){
+  table.setData("https://129.213.48.136/api/area/");
 });
